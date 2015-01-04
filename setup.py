46d5ba03591d2ee4d3936860e0cc19db8c9a7003
kAlmAcetA <kalmaceta@gmail.com>
2015-01-04 03:54:43 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -23,9 +23,14 @@ setup(
     license="MIT",
     classifiers=[
         'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Operating System :: POSIX',
-        'Development Status :: 3 - Alpha'
+        'Development Status :: 3 - Beta'
     ]
 )
