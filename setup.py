638f7356db0de124a8b33a2999ef39177455005d
kAlmAcetA <kalmaceta@gmail.com>
2015-01-05 07:26:22 +0100

    fix: beta classifier

--- setup.py
+++ setup.py
@@ -31,6 +31,6 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Operating System :: POSIX',
-        'Development Status :: 3 - Beta'
+        'Development Status :: 4 - Beta'
     ]
 )
